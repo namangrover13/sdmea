a7f6a587e44c829becc34aa4b67e2179511e10fb
vipuldev <vipul.xtr@gmail.com>
2021-09-02 23:18:38 -0700

    adding react-router-dom , axios , material-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Login from './pages/Login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <Switch>
+          <Route path="/" exact>
+              <Home />
+          </Route>
+          <Route path="/dashboard">
+              <Dashboard />
+          </Route>
+<Route path="/login">
+    <Login />
+</Route>
+      </Switch>
+      </Router>
   );
 }
 
