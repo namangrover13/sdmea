d047ff8bb758c58ae5d86986b0a32de56a8d2022
Naman Grover <namangrover.1399@gmail.com>
2021-09-05 01:28:40 +0530

    Completed header for HomePage

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -7,10 +7,55 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         justifyContent: 'space-between',
         background: theme.palette.primary.main,
+        fontFamily: 'Cabin, sans-serif',
+        alignContent: 'center',
         margin: 0,
-        padding: 0
+        padding: '5px 10px',
     },
-
+    navItems: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignContent: 'center',
+        padding: 0,
+        margin:0,
+        marginLeft:10
+    },
+    navItem: {
+        color: theme.palette.primary.text,
+        marginLeft: 20,
+        fontSize: 20,
+        textTransform: 'uppercase',
+        cursor: 'pointer',
+        padding:0
+    },
+    logo: {
+        background: theme.palette.primary.text,
+        height:60,
+        width: 60,
+        borderRadius: '50%',
+        margin: 'auto',
+        cursor: 'pointer'
+    },
+    logoTxt: {
+        textAlign: 'center',
+        color: theme.palette.primary.main,
+        display: 'block',
+        padding: 'auto',
+        fontWeight:800,
+    },
+    navBtns: {
+        margin: 'auto 2px'
+    },
+    btn: {
+        color: theme.palette.primary.main,
+        background: theme.palette.common.white,
+        margin: 'auto 10px',
+        fontWeight: 600,
+        '&:hover':{
+            background: theme.palette.common.white,
+            boxShadow: '3px 3px 3px rgba(255, 255, 255, 0.5)'
+        }
+    }
 }))
 
 const HomeHeader = () => {
@@ -18,16 +63,16 @@ const HomeHeader = () => {
     return (
         <div className={classes.container}>
             <div className={classes.navItems}>
-                <p>One</p>
-                <p>Two</p>
-                <p>Three</p>
+                <p className={classes.navItem}>One</p>
+                <p className={classes.navItem}>Two</p>
+                <p className={classes.navItem}>Three</p>
             </div>
             <div className={classes.logo}>
-                SDMEA
+                <p className={classes.logoTxt}>SDMEA</p>
             </div>
             <div className={classes.navBtns}>
-                <Button>Signup</Button>
-                <Button>Login</Button>
+                <Button className={classes.btn}>Signup</Button>
+                <Button className={classes.btn}>Login</Button>
             </div>
         </div>
     )
