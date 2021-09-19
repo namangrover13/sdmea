b70e041e7b48c29f85cacea51b3aad6ca5715464
Naman Grover <namangrover.1399@gmail.com>
2021-09-20 01:09:25 +0530

    Added Popup and form in Modify

--- src/components/Screens/Modify.jsx
+++ src/components/Screens/Modify.jsx
@@ -1,54 +1,129 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { useStyles } from './css/Modify.css';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@material-ui/core';
+import Popup from '../GlobalComponents/Popup';
+
+const ModifyForm = (props) => {
+  const data = props.data
+  // const handleChange = (event) => {
+  //   setValue(event.target.value);
+  // };
+  return(
+    <div style={{display: 'flex', flexDirection: 'column', padding: 25}}>
+      <TextField
+          style={{margin: 20}}
+          id="name"
+          label="Name"
+          defaultValue={data.name}
+          value={data.name}
+          // onChange={handleChange}
+          helperText="Click to change Name"
+        />
+      <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly'}}>
+        <TextField
+          style={{margin: 20}}
+          id="gender"
+          label="Gender"
+          defaultValue={data.gender}
+          value={data.gender}
+          // onChange={handleChange}
+          helperText="Click to change Gender"
+        />
+        <TextField
+          style={{margin: 20}}
+          id="age"
+          label="Age"
+          defaultValue={data.age}
+          value={data.age}
+          // onChange={handleChange}
+          helperText="Click to change Age"
+        />
+        </div>
+        <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly'}}>
+        <TextField
+          style={{margin: 20}}
+          id="tenthPercent"
+          label="10 %"
+          defaultValue={data.tenthPercent}
+          value={data.tenthPercent}
+          // onChange={handleChange}
+          helperText="Click to change 10th Percent"
+        />
+        <TextField
+          style={{margin: 20}}
+          id="standard"
+          label="Standard"
+          defaultValue={data.standard}
+          value={data.standard}
+          // onChange={handleChange}
+          helperText="Click to change Standard"
+        />
+      </div>
+      <Button className={props.btnClass} onClick={props.handleClose}>Done</Button>
+    </div>
+  )
+}
 
 const Modify = ({
-    students
+  students
 }) => {
-    const classes = useStyles();
-    
-    return (
-        <div className={classes.root}>
-            <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 300 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Name</TableCell>
-            <TableCell align="center">Gender</TableCell>
-            <TableCell align="center">Age</TableCell>
-            <TableCell align="center">Standard</TableCell>
-            <TableCell align="center">10 %</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {students.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="left" component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="center">{row.gender}</TableCell>
-              <TableCell align="center">{row.age}</TableCell>
-              <TableCell align="center">{row.standard}</TableCell>
-              <TableCell align="center">{row.tenthPercent}</TableCell>
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState();
+  const handleClick = (row) => {
+    setSelected(row)
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+  return (
+    <div className={classes.root}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 300 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Name</TableCell>
+              <TableCell align="center">Gender</TableCell>
+              <TableCell align="center">Age</TableCell>
+              <TableCell align="center">Standard</TableCell>
+              <TableCell align="center">10 %</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {students.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                className={classes.tableRow}
+                onClick={() => handleClick(row)}
+              >
+                <TableCell align="left" component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.gender}</TableCell>
+                <TableCell align="center">{row.age}</TableCell>
+                <TableCell align="center">{row.standard}</TableCell>
+                <TableCell align="center">{row.tenthPercent}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Popup open={open} onClose={handleClose} heading={"Edit Form"} >
+              <ModifyForm data={selected} handleClose={handleClose} btnClass={classes.btnClass} />
+      </Popup>
 
-        </div>
-    )
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
-    return{
-        students: state.students
-    }
+  return {
+    students: state.students
+  }
 }
 
 export default withRouter(connect(mapStateToProps)(Modify));
